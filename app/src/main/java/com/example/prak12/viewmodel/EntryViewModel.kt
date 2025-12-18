93ef49903729870204a4be685e6c66f93f50cc58
nabilasaf <nabilasafitri861@gmail.com>
2025-12-18 10:39:37 +0700

    menambahkan entry view model

---
+++ app/src/main/java/com/example/prak12/viewmodel/EntryViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.prak12.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.prak12.modeldata.DetailSiswa
+import com.example.prak12.modeldata.UIStateSiswa
+import com.example.prak12.modeldata.toDataSiswa
+import com.example.prak12.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+
+}
