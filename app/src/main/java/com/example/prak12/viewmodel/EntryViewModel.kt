8dcf989535178f57ce82fa795c2e20f4ed5e9660
nabilasaf <nabilasafitri861@gmail.com>
2025-12-18 10:41:46 +0700

    menambahkan Fungsi untuk menangani saat ada perubahan pada text input ke entry view model

--- app/src/main/java/com/example/prak12/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/prak12/viewmodel/EntryViewModel.kt
@@ -22,4 +22,14 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+    //Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa, isEntryValid = validasiInput
+                    (detailSiswa)
+            )
     }
+
+
+}
