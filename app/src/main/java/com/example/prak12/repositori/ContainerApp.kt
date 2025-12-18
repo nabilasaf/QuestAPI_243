9d4a3fdcb8512f4c7c35f8fa788e00da1c7b5a88
nabilasaf <nabilasafitri861@gmail.com>
2025-12-18 15:07:32 +0700

    memperbaiki container

--- app/src/main/java/com/example/prak12/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/prak12/repositori/ContainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.prak12.repositori
 
+import android.app.Application
 import com.example.prak12.apiservice.ServiceApiSiswa
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -10,45 +11,45 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.create
 
-interface ContainerApp{
-    val repositorySiswa: RepositoryDataSiswa
+interface ContainerApp {
+    val repositoryDataSiswa : RepositoryDataSiswa
 }
 
-class DefaultAppContainer: ContainerApp {
-    private val baseUrl = "http://10.0.2.2/umyTI/"
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = "http://10.0.2.2/umyIT/"
 
     val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
-
     val klien = OkHttpClient.Builder()
         .addInterceptor(logging)
         .build()
 
-    private val retrofit : Retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
         .addConverterFactory(
-            Json{
+            Json {
                 ignoreUnknownKeys = true
                 prettyPrint = true
                 isLenient = true
-            } .asConverterFactory("aplication/json".toMediaType())
+            }.asConverterFactory("application/json".toMediaType())
         )
         .client(klien)
         .build()
 
-    private val retrofitService: ServiceApiSiswa by lazy {
+    private val retrofitService : ServiceApiSiswa by lazy {
         retrofit.create(ServiceApiSiswa::class.java)
     }
-    override val repositorySiswa: RepositoryDataSiswa by lazy {
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
         JaringanRepositoryDataSiswa(retrofitService)
     }
 }
 
-class AplikasiDataSiswa : Aplication(){
-    lateinit var container: ContainerApp
-    override fun onCreate(){
+class AplikasiDataSiswa : Application() {
+    lateinit var container : ContainerApp
+    override fun onCreate() {
         super.onCreate()
-        this.container = DefaultAppContainer()
+        this.container = DefaultContainerApp()
     }
 }
