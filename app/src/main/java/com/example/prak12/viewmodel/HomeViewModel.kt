e76b04d7256e36a297f26f1290532238b9fea4db
nabilasaf <nabilasafitri861@gmail.com>
2025-12-18 10:42:34 +0700

    melengkapi home view model

--- app/src/main/java/com/example/prak12/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/prak12/viewmodel/HomeViewModel.kt
@@ -17,3 +17,25 @@ sealed interface StatusUiSiswa {
     object Loading : StatusUiSiswa
 }
 
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel() {
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e:IOException){
+                StatusUiSiswa.Error
+            }catch (e:HttpException){
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
