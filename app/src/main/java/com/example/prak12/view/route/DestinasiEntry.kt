601b8cfa360df6b1588646af73943ceac33f5351
nabilasaf <nabilasafitri861@gmail.com>
2025-12-18 15:07:50 +0700

    menambahkan destinasi

---
+++ app/src/main/java/com/example/prak12/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.prak12.view.route
+
+import com.example.prak12.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
---
+++ app/src/main/java/com/example/prak12/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak12.view.route
+
+import com.example.prak12.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
