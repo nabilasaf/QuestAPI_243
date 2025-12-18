a675214475d0e63122684689d93e1896c3492a36
nabilasaf <nabilasafitri861@gmail.com>
2025-12-18 15:08:31 +0700

    menambahkan penyedia view model

--- app/src/main/java/com/example/prak12/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak12/viewmodel/provider/PenyediaViewModel.kt
@@ -1,19 +1,31 @@
 package com.example.prak12.viewmodel.provider
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak12.repositori.AplikasiDataSiswa
+import com.example.prak12.viewmodel.EntryViewModel
+import com.example.prak12.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as
                 AplikasiDataSiswa
         )
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container
-            .repositoryDataSiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container
-            .repositoryDataSiswa) }
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
     }
 }
