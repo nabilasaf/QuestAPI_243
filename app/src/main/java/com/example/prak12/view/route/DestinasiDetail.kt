acc9c38a7378fb662f326497f96587a637df7a9b
nabilasaf <nabilasafitri861@gmail.com>
2025-12-25 09:26:44 +0700

    menambahkan destinasi detail

---
+++ app/src/main/java/com/example/prak12/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.prak12.view.route
+
+import com.example.prak12.R
+
+object DestinasiDetail {
+    val route = "item_detail"
+    val titleRes = R.string.detail_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
